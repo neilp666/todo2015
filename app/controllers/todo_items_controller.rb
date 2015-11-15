37aba735d9d944531d94148074e76e9dc3060ef2
neil patel <neil@myindianweddingapp.com>
2015-11-15 11:33:08 +0000

    Extract todo list helper

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,6 @@ class TodoItemsController < ApplicationController
 
   before_action :find_todo_list
 
-
   def index
   end
 
@@ -38,6 +37,16 @@ class TodoItemsController < ApplicationController
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
+  def destroy
+      @todo_item = @todo_list.todo_items.find(params[:id])
+      if @todo_item.destroy
+         flash[:success] = "Todo list item was deleted."
+      else
+         flash[:error] = "Todo list item could not be deleted."
+      end
+         redirect_to todo_list_todo_items_path
+  end
+
   def url_options
     { todo_list_id: params[:todo_list_id] }.merge(super)
   end
@@ -47,6 +56,7 @@ class TodoItemsController < ApplicationController
 
   def find_todo_list
      @todo_list = TodoList.find(params[:todo_list_id])
+  end
 
     def todo_item_params
       params[:todo_item].permit(:content)
